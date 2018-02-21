19910fbce4f648bcde220de2d6e2b07aa185486b
Paul Seyfert <pseyfert@cern.ch>
2018-02-21 19:08:54 +0100

    intermediate snapshot

--- complex_bench.cc
+++ complex_bench.cc
@@ -5,25 +5,43 @@
 #include <llvm/ADT/SmallVector.h>
 #include <vector>
 
+#define make_full 1
+#define always_reserve 1
+#undef  overfill
+#undef  underfill
+
 constexpr static int CONTAINERSIZE = 30;
-constexpr static int N_BIGTHINGS = 100;
+constexpr static int FILLING_OFFSET = 8;
 typedef int INNER_DATATYPE;
+constexpr static int N_BIGTHINGS = 100;
+
 template <typename BIGTHING_TYPE>
 using OUTER_CONTAINER_TYPE = std::vector<BIGTHING_TYPE>;
 
 
 
 
-
-
-
-
+auto compute_min = []( const std::vector<double>& v ) -> double {
+  return *( std::min_element( std::begin( v ), std::end( v ) ) );
+};
 
 template <typename CONTAINER>
 struct bigthing {
   bigthing(size_t N) {
-    for (size_t ii = 0 ; ii < CONTAINERSIZE ; ++ii) {
-      m_collection.push_back(N%(1+ii));
+#ifdef make_full
+    constexpr int loopend = CONTAINERSIZE;
+#endif
+#ifdef overfill
+    constexpr int loopend = CONTAINERSIZE + FILLING_OFFSET;
+#endif
+#ifdef underfill
+    constexpr int loopend = CONTAINERSIZE - FILLING_OFFSET;
+#endif
+#ifdef always_reserve
+    m_collection.reserve(loopend);
+#endif
+    for ( size_t i = 0; i < loopend; i++ ) {
+      m_collection.push_back(N%(1+i));
     }
   }
   CONTAINER m_collection;
@@ -47,19 +65,26 @@ static void create_and_push( benchmark::State& state )
   }
 }
 
-static void test_std_vector         ( benchmark::State& state ) { create_and_push<std::vector<INNER_DATATYPE>>( state ); }
-static void test_boost_small_vector ( benchmark::State& state ) { create_and_push<boost::container::small_vector<INNER_DATATYPE, CONTAINERSIZE>>( state ); }
-static void test_boost_static_vector( benchmark::State& state ) { create_and_push<boost::container::static_vector<INNER_DATATYPE, CONTAINERSIZE>>( state ); }
-static void test_llvm_small_vector  ( benchmark::State& state ) { create_and_push<llvm::SmallVector<INNER_DATATYPE, CONTAINERSIZE>>( state ); }
-static void test_absl_inline_vector ( benchmark::State& state ) { create_and_push<absl::InlinedVector<INNER_DATATYPE, CONTAINERSIZE>>( state ); }
 
 
 template <typename CONTAINER>
 struct reservething {
   reservething(size_t N) {
-    m_collection.reserve(N);
-    for (size_t ii = 0 ; ii < CONTAINERSIZE ; ++ii) {
-      m_collection.push_back(N%(1+ii));
+    m_collection.reserve( CONTAINERSIZE );
+#ifdef make_full
+    constexpr int loopend = CONTAINERSIZE;
+#endif
+#ifdef overfill
+    constexpr int loopend = CONTAINERSIZE + FILLING_OFFSET;
+#endif
+#ifdef underfill
+    constexpr int loopend = CONTAINERSIZE - FILLING_OFFSET;
+#endif
+#ifdef always_reserve
+    m_collection.reserve(loopend);
+#endif
+    for ( size_t i = 0; i < loopend; i++ ) {
+      m_collection.push_back(N%(1+i));
     }
   }
   CONTAINER m_collection;
@@ -77,32 +102,13 @@ static void create_and_push_and_reserve( benchmark::State& state )
   }
 }
 
-BENCHMARK( test_std_vector         )
-    ->ComputeStatistics( "min", []( const std::vector<double>& v ) -> double {
-      return *( std::min_element( std::begin( v ), std::end( v ) ) );
-    } );
-BENCHMARK( test_boost_small_vector )
-    ->ComputeStatistics( "min", []( const std::vector<double>& v ) -> double {
-      return *( std::min_element( std::begin( v ), std::end( v ) ) );
-    } );
-
-// BAD ALLOC
-
-BENCHMARK( test_boost_static_vector)
-    ->ComputeStatistics( "min", []( const std::vector<double>& v ) -> double {
-      return *( std::min_element( std::begin( v ), std::end( v ) ) );
-    } );
-BENCHMARK( test_llvm_small_vector  )
-    ->ComputeStatistics( "min", []( const std::vector<double>& v ) -> double {
-      return *( std::min_element( std::begin( v ), std::end( v ) ) );
-    } );
-BENCHMARK( test_absl_inline_vector )
-    ->ComputeStatistics( "min", []( const std::vector<double>& v ) -> double {
-      return *( std::min_element( std::begin( v ), std::end( v ) ) );
-    } );
-BENCHMARK( create_and_push_and_reserve )
-    ->ComputeStatistics( "min", []( const std::vector<double>& v ) -> double {
-      return *( std::min_element( std::begin( v ), std::end( v ) ) );
-    } );
+BENCHMARK_TEMPLATE( create_and_push, std::vector                    <INNER_DATATYPE               > )->ComputeStatistics( "min", compute_min )->ThreadRange(1,4);
+BENCHMARK_TEMPLATE( create_and_push, boost::container::small_vector <INNER_DATATYPE, CONTAINERSIZE> )->ComputeStatistics( "min", compute_min )->ThreadRange(1,4);
+#ifndef overfill
+BENCHMARK_TEMPLATE( create_and_push, boost::container::static_vector<INNER_DATATYPE, CONTAINERSIZE> )->ComputeStatistics( "min", compute_min )->ThreadRange(1,4);
+#endif
+BENCHMARK_TEMPLATE( create_and_push, llvm::SmallVector              <INNER_DATATYPE, CONTAINERSIZE> )->ComputeStatistics( "min", compute_min )->ThreadRange(1,4);
+BENCHMARK_TEMPLATE( create_and_push, absl::InlinedVector            <INNER_DATATYPE, CONTAINERSIZE> )->ComputeStatistics( "min", compute_min )->ThreadRange(1,4);
+BENCHMARK(          create_and_push_and_reserve                                                     )->ComputeStatistics( "min", compute_min )->ThreadRange(1,4);
 
 BENCHMARK_MAIN();
